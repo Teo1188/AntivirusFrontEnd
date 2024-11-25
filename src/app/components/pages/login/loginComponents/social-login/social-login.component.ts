037dd849df998b27aaa14ea7483ebe94c1d3bbf0
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-11-25 00:59:26 -0500

    Ajustes login con facebook y google.

--- src/app/components/pages/login/loginComponents/social-login/social-login.component.ts
+++ src/app/components/pages/login/loginComponents/social-login/social-login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthSocialMediaService } from '../../auth-social-media.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { SweetalertService } from 'src/app/sweetalert.service';
 
 @Component({
   selector: 'app-social-login',
@@ -7,27 +9,40 @@ import { AuthSocialMediaService } from '../../auth-social-media.service';
   styleUrls: ['./social-login.component.css']
 })
 export class SocialLoginComponent {
-  constructor(
-    private authSocialMediaService: AuthSocialMediaService
-){}
-signInWithGoogle() {
-  this.authSocialMediaService
-    .signInWithGoogle()
-    .then((result) => {
-      console.log('User signed in:', result.user);
-    })
-    .catch((error) => {
-      console.error('Error signing in with Google:', error);
-      });
-  }
-loginWithFacebook() {
-  this.authSocialMediaService.signInWithFacebook()
-    .then((result) => {
-      console.log('Logged in with Facebook:', result);
-    })
-    .catch((error) => {
-      console.error('Error logging in with Facebook:', error);
-      });
-  }
-}
+  constructor(private authSocialMediaService: AuthSocialMediaService, private authService: AuthService, private sweetalertService: SweetalertService) { }
 
+  signInWithGoogle() {
+    this.authSocialMediaService
+      .signInWithGoogle()
+      .then((result) => {
+        console.log('User signed in:', result.user);
+        if (result && result.user && result.user.getIdToken) {
+          result.user.getIdToken().then(token => {
+            this.authService.saveToken(token);
+            console.log('Token guardado');
+            this.sweetalertService.showSuccessLogin('Login exitoso. ¿Te gustaría ir a las novedades?');
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error signing in with Google:', error);
+      });
+  }
+
+  loginWithFacebook() {
+    this.authSocialMediaService.signInWithFacebook()
+      .then((result) => {
+        console.log('Logged in with Facebook:', result);
+        if (result && result.user && result.user.getIdToken) {
+          result.user.getIdToken().then(token => {
+            this.authService.saveToken(token);
+            console.log('Token guardado');
+            this.sweetalertService.showSuccessLogin('Login exitoso. ¿Te gustaría ir a las novedades?');
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error logging in with Facebook:', error);
+      });
+  }
+}
